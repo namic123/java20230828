1c5220c827d58a9b2b15027b8d6b9d4bccfdf4c7
jay sung <namic123@naver.com>
2023-09-19 16:42:01 +0900

    Test git

---
+++ src/ch12api/lecture/C12string.java
@@ -0,0 +1,11 @@
+package ch12api.lecture;
+
+public class C12string {
+    public static void main(String[] args) {
+        String a = "java";
+        String b = "spring";
+        String c = "hello world";
+
+
+    }
+}
